9acff93f5c37deffe1cc1d5215abe5b27c41af3e
sergxlove <sergxlove@mail.ru>
2026-01-05 17:46:54 +0600

    create models

---
+++ ProgressOS.Core/Models/Users.cs
@@ -0,0 +1,23 @@
+﻿using ProgressOS.Core.Infrastructures;
+
+namespace ProgressOS.Core.Models
+{
+    public class Users
+    {
+        public Guid Id { get; }
+        public string Nickname { get; } = string.Empty;
+
+        public static ResultCreateModel<Users> Create(Guid id, string nickname)
+        {
+
+            return ResultCreateModel<Users>.Success(new(id, nickname));
+        }
+
+        private Users(Guid id, string nickname)
+        {
+            Id = id;
+            Nickname = nickname;
+        }
+
+    }
+}
