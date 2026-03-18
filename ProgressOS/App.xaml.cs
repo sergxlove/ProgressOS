80c668b8290d0f54c401b3b76adc17755c952f30
sergxlove <sergxlove@mail.ru>
2026-03-18 19:26:02 +0600

    add main page ui

--- ProgressOS/App.xaml.cs
+++ ProgressOS/App.xaml.cs
@@ -7,7 +7,7 @@ namespace ProgressOS
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : System.Windows.Application
     {
     }
 
