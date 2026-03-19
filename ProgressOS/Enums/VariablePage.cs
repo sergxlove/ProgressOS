534333c551015ea163b92a428b7674223931fc7a
sergxlove <sergxlove@mail.ru>
2026-03-19 18:20:03 +0600

    update ui, add navigation page

---
+++ ProgressOS/Enums/VariablePage.cs
@@ -0,0 +1,11 @@
+﻿namespace ProgressOS.Enums
+{
+    public enum VariablePage
+    {
+        None = 0,
+        Main = 1, 
+        GoalsDays = 2,
+        GoalsYear = 3,
+        Settings = 4
+    }
+}
